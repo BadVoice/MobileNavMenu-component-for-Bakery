2ed89cd93dc4752b65ca190e4c22eb3319e49ebe
Herman <smurovnemez@mail.ru>
2022-10-16 21:51:33 -0500

    feat: add first column in mobileNav

--- src/components/NavMenu/NavMenu.vue
+++ src/components/NavMenu/NavMenu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="navMenu container  max-w-sm w-[100%] h-[100vh] bg-white
+    <div class="navMenu container  max-w-m w-[100%] h-[100vh] bg-white
      overflow-y-scroll bg-gray-900 
 
     ">
@@ -17,15 +17,49 @@
            
                 <div class="relative">
                     <div class="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
-
+                        <svg class="w-4 h-4 text-gray-500 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                        </svg>
+                        
                     </div>
                     <input type="text" class="w-full rounded pl-8 pr-4 py-2.5 text-xs font-xl text-[#E1CEAC] bg-white placeholder-[#E1CEAC] focus:outline-none focus:ring-1 focus:ring-gray-300 " placeholder="search">
                 </div>
-            </div>  
-        
 
+                <div class="pt-4">
+                    
+                </div>
+        </div>  
+
+        <div class="px-6 pt-4 ">
+            <ul class="flex flex-col space-y-2">
+                <li class="">
+                    <div class="relative flex justify-between text-black bg-[#E1CEAC] rounded">
+                        <div class="flex items-center w-full">
+                            <div class="pr-0.5 inset-y-0 left-0 flex items-center pl-2 pointer-events-none text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
+                                  </svg>
+                                  
+                            </div>
+                            <RouterLink to="/" class=" inline-block w-full py-2 pl-2 pr-2 text-xs rounded hover:bg-[#E1CEAC] focus:outline-none focus:bg-[#E1CEAC] focus:bg-[#E1CEAC] focus:bg-[#E1CEAC]" href="">
+                                Cailcut Shop
+                            </RouterLink>
+                        </div> 
+                        <button class=" flex items-center p-1 text-white">
+                            <svg class="w-5 h-5 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                              </svg>
+                              
+                        </button>
+                    </div>
+                    
+                </li>
+            </ul>
+        </div>
         
-        <hr class="h-1 bg-[#E0E0E0]">
+            
+        
+        
 
     </div>
 </template>
